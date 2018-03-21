28b05a8cec775ac32dc5bc5a55a4778b7748071a
David Neustadt <saberdude@googlemail.com>
2018-03-22 00:28:53 +0100

    filter for event and plugin logs

--- Resources/views/frontend/_public/src/js/jquery.pub-sub-debug.js
+++ Resources/views/frontend/_public/src/js/jquery.pub-sub-debug.js
@@ -33,11 +33,18 @@
                     case 'subscribe':
                     case 'unsubscribe':
                     case 'publish':
-                        if (type === 'publish' && arguments[1]) {
-                            console.log("(%s)%s %s [arguments: %O]", type, indents, arguments[0], arguments[1]);
+                        var eventName = arguments[0],
+                            args = arguments[1];
+
+                        if (me.filter.event.length !== 0 && !me.filter.event.some(function (val) { return eventName.includes(val) })) {
+                            break;
+                        }
+
+                        if (type === 'publish' && args) {
+                            console.log("(%s)%s %s [arguments: %O]", type, indents, eventName, args);
                         }
                         else {
-                            console.log("(%s)%s %s", type, indents, arguments[0]);
+                            console.log("(%s)%s %s", type, indents, eventName);
                         }
 
                         break;
@@ -45,16 +52,27 @@
                     case 'removePlugin':
                     case 'updatePlugin':
                     case 'destroyPlugin':
-                        if (type === 'addPlugin' && arguments[2]) {
-                            console.log("(%s)%s %s [element: %o, viewports: %O]", type, indents, arguments[1], arguments[0], arguments[2]);
+                        var element = arguments[0],
+                            pluginName = arguments[1],
+                            viewports = arguments[2];
+
+                        if (me.filter.plugin.length !== 0 && !me.filter.plugin.some(function (val) { return pluginName.includes(val) })) {
+                            break;
+                        }
+
+                        if (type === 'addPlugin' && viewports) {
+                            console.log("(%s)%s %s [element: %o, viewports: %O]", type, indents, pluginName, element, viewports);
                         }
                         else {
-                            console.log("(%s)%s %s [element: %o]", type, indents, arguments[1], arguments[0]);
+                            console.log("(%s)%s %s [element: %o]", type, indents, pluginName, element);
                         }
 
                         break;
                     case 'switchPlugins':
-                        console.log("(%s)%s %c[previousState: %s, currentState: %s]", type, indents, 'font-weight:bold', arguments[0], arguments[1]);
+                        var previousState = arguments[0],
+                            currentState = arguments[1];
+
+                        console.log("(%s)%s %c[previousState: %s, currentState: %s]", type, indents, 'font-weight:bold', previousState, currentState);
 
                         break;
                     case 'initPlugin':
@@ -67,6 +85,10 @@
                             selector = arguments[1],
                             pluginName = arguments[2],
                             plugin = element.data('plugin_' + pluginName);
+
+                        if (me.filter.plugin.length !== 0 && !me.filter.plugin.some(function (val) { return pluginName.includes(val) })) {
+                            return;
+                        }
 
                         if (plugin) {
                             console.log("(%s)%s %s [selector: %o, events: %O]", type, indents, pluginName, selector, plugin._events);
