a1c40678730b3d841520cb3704dd57ab72f75868
David Neustadt <saberdude@googlemail.com>
2018-03-21 12:13:51 +0100

    switchPlugin logs

--- Resources/views/frontend/_public/src/js/jquery.pub-sub-debug.js
+++ Resources/views/frontend/_public/src/js/jquery.pub-sub-debug.js
@@ -17,12 +17,18 @@
                     break;
                 case 'addPlugin':
                 case 'removePlugin':
+                case 'updatePlugin':
+                case 'destroyPlugin':
                     if (type === 'addPlugin' && arguments[2]) {
                         console.log("(%s)\t\t %s [element: %o, viewports: %O]", type, arguments[1], arguments[0], arguments[2]);
                     }
                     else {
                         console.log("(%s)\t %s [element: %o]", type, arguments[1], arguments[0]);
                     }
+
+                    break;
+                case 'switchPlugins':
+                    console.info("(%s)\t %c[previousState: %s, currentState: %s]", type, 'font-weight:bold', arguments[0], arguments[1]);
 
                     break;
             }
@@ -41,5 +47,9 @@
 
     window.StateManager.addPlugin = $.debugPubSub(window.StateManager.addPlugin, 'addPlugin');
     window.StateManager.removePlugin = $.debugPubSub(window.StateManager.removePlugin, 'removePlugin');
+    window.StateManager.updatePlugin = $.debugPubSub(window.StateManager.removePlugin, 'updatePlugin');
+    window.StateManager.destroyPlugin = $.debugPubSub(window.StateManager.removePlugin, 'destroyPlugin');
+
+    window.StateManager._switchPlugins = $.debugPubSub(window.StateManager._switchPlugins, 'switchPlugins');
 
 }(jQuery));
