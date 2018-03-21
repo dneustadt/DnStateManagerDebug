5b23fd80ae2e4ace3c09a18173fc90bcba56ee3c
David Neustadt <saberdude@googlemail.com>
2018-03-21 01:31:33 +0100

    console log addPlugin and removePlugin calls

--- Resources/views/frontend/_public/src/js/jquery.pub-sub-debug.js
+++ Resources/views/frontend/_public/src/js/jquery.pub-sub-debug.js
@@ -4,12 +4,30 @@
 
         return function() {
             var log = {
-                'type': type,
-                'event': arguments[0]
+                'type': type
             };
 
-            if (type === 'publish') {
-                log.arguments = arguments[1];
+            switch(type) {
+                case 'subscribe':
+                case 'unsubscribe':
+                case 'publish':
+                    log.event = arguments[0];
+
+                    if (type === 'publish' && arguments[1]) {
+                        log.arguments = arguments[1];
+                    }
+
+                    break;
+                case 'addPlugin':
+                case 'removePlugin':
+                    log.selector = arguments[0];
+                    log.plugin = arguments[1];
+
+                    if (type === 'addPlugin' && arguments[2]) {
+                        log.viewports = arguments[2];
+                    }
+
+                    break;
             }
 
             console.log(log);
@@ -25,5 +43,8 @@
     $.subscribe = $.debugPubSub($.subscribe, 'subscribe');
     $.unsubscribe = $.debugPubSub($.unsubscribe, 'unsubscribe');
     $.publish = $.debugPubSub($.publish, 'publish');
+
+    window.StateManager.addPlugin = $.debugPubSub(window.StateManager.addPlugin, 'addPlugin');
+    window.StateManager.removePlugin = $.debugPubSub(window.StateManager.removePlugin, 'removePlugin');
 
 }(jQuery));
