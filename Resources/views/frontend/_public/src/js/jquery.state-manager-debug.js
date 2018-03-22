6b127fc23b442c1db3dc6307ba76391d122d9e17
David Neustadt <saberdude@googlemail.com>
2018-03-22 20:11:34 +0100

    log functions

--- Resources/views/frontend/_public/src/js/jquery.state-manager-debug.js
+++ Resources/views/frontend/_public/src/js/jquery.state-manager-debug.js
@@ -33,46 +33,30 @@
                     case 'subscribe':
                     case 'unsubscribe':
                     case 'publish':
-                        var eventName = arguments[0],
-                            args = arguments[1];
+                        var eventName = arguments[0];
 
                         if (me.filter.event.length !== 0 && !me.filter.event.some(function (val) { return eventName.includes(val) })) {
                             break;
                         }
 
-                        if (type === 'publish' && args) {
-                            console.log("(%s)%s %s [arguments: %O]", type, indents, eventName, args);
-                        }
-                        else {
-                            console.log("(%s)%s %s", type, indents, eventName);
-                        }
+                        me.logEvents(type, eventName, indents, arguments);
 
                         break;
                     case 'addPlugin':
                     case 'removePlugin':
                     case 'updatePlugin':
                     case 'destroyPlugin':
-                        var element = arguments[0],
-                            pluginName = arguments[1],
-                            viewports = arguments[2];
+                        var pluginName = arguments[1];
 
                         if (me.filter.plugin.length !== 0 && !me.filter.plugin.some(function (val) { return pluginName.includes(val) })) {
                             break;
                         }
 
-                        if (type === 'addPlugin' && viewports) {
-                            console.log("(%s)%s %s [element: %o, viewports: %O]", type, indents, pluginName, element, viewports);
-                        }
-                        else {
-                            console.log("(%s)%s %s [element: %o]", type, indents, pluginName, element);
-                        }
+                        me.logPlugins(type, pluginName, indents, arguments);
 
                         break;
                     case 'switchPlugins':
-                        var previousState = arguments[0],
-                            currentState = arguments[1];
-
-                        console.log("(%s)%s %c[previousState: %s, currentState: %s]", type, indents, 'font-weight:bold', previousState, currentState);
+                        me.logBreakpoints(type, indents, arguments);
 
                         break;
                     case 'initPlugin':
@@ -81,21 +65,13 @@
                             arguments
                         );
 
-                        var element = arguments[0],
-                            selector = arguments[1],
-                            pluginName = arguments[2],
-                            plugin = element.data('plugin_' + pluginName);
+                        var pluginInitName = arguments[2];
 
-                        if (me.filter.plugin.length !== 0 && !me.filter.plugin.some(function (val) { return pluginName.includes(val) })) {
+                        if (me.filter.plugin.length !== 0 && !me.filter.plugin.some(function (val) { return pluginInitName.includes(val) })) {
                             return;
                         }
 
-                        if (plugin) {
-                            console.log("(%s)%s %s [selector: %o, events: %O]", type, indents, pluginName, selector, plugin._events);
-                        }
-                        else {
-                            console.log("(%s)%s %s [selector: %o]", type, indents, pluginName, selector);
-                        }
+                        me.logInits(type, pluginInitName, indents, arguments);
 
                         return;
                 }
@@ -104,6 +80,49 @@
                     this,
                     arguments
                 );
+            }
+        },
+
+        logEvents: function (type, eventName, indents, args) {
+            var argsArray = args[1];
+
+            if (type === 'publish' && args) {
+                console.log("(%s)%s %s [arguments: %O]", type, indents, eventName, argsArray);
+            }
+            else {
+                console.log("(%s)%s %s", type, indents, eventName);
+            }
+        },
+
+        logPlugins: function (type, pluginName, indents, args) {
+            var element = args[0],
+                viewports = args[2];
+
+            if (type === 'addPlugin' && viewports) {
+                console.log("(%s)%s %s [element: %o, viewports: %O]", type, indents, pluginName, element, viewports);
+            }
+            else {
+                console.log("(%s)%s %s [element: %o]", type, indents, pluginName, element);
+            }
+        },
+
+        logBreakpoints: function (type, indents, args) {
+            var previousState = args[0],
+                currentState = args[1];
+
+            console.log("(%s)%s %c[previousState: %s, currentState: %s]", type, indents, 'font-weight:bold', previousState, currentState);
+        },
+
+        logInits: function (type, pluginName, indents, args) {
+            var element = args[0],
+                selector = args[1],
+                plugin = element.data('plugin_' + pluginName);
+
+            if (plugin) {
+                console.log("(%s)%s %s [selector: %o, events: %O]", type, indents, pluginName, selector, plugin._events);
+            }
+            else {
+                console.log("(%s)%s %s [selector: %o]", type, indents, pluginName, selector);
             }
         },
 
